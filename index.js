8b01c09110f7d2e4c5ce19c507152c381ba0da94
LabajosHejteros <labaj.fipo@gmail.com>
2023-10-30 10:17:51 +0100

    1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express'); // Import frameworku Express
 const app = express(); // Vytvoření instance aplikace Express
 const port = 3000; // Port, na kterém server běží
 
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,6 +11,8 @@ const ejs = require('ejs'); // Import šablony EJS pro renderování HTML
 
 app.set('view engine', 'ejs'); // Nastavuje templating engine na EJS (Embedded JavaScript).
 app.set('views', path.join(__dirname, 'views')); //  Nastavuje cestu pro hledání šablon (views).
+app.use(express.static(path.join(__dirname, 'images')));
+
 
 // Spuštění serveru na zvoleném portu
 app.listen(port, () => {
